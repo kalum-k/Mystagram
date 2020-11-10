59be51f757ccc6b2ce348ff9a1ba14fa75508a43
Sirinrat <614259029@webmail.npru.ac.th>
2020-11-10 22:15:50 +0800

    API1

--- public/app.js
+++ public/app.js
@@ -1,15 +1,17 @@
 const addImagestoGallery = (res) => {
-    const gallery = document.querySelector(".gallery");
-    let html = "";
-    res.results.forEach((element) => {
-        html += ` 
+  const gallery = document.querySelector(".gallery");
+  let html = "";
+  res.results.forEach((element) => {
+    html += ` 
     <section class="photo">
     <header class="photo__header">
       <div class="photo__header-color">
+      <a href="profile.html?username=${element.user.username}">
         <img class="photo__avatar" src="${element.user.profile_image.medium}"/>
+      </a>
       </div>
       <div class="photo__header-column">
-        <span class="photo__username">${element.user.username}</span>
+        <span class="photo__username"><a href="profile.html?username=${element.user.username}">${element.user.username}</a></span>
       </div>
     </header>
     <div class="photo__file-container">
@@ -23,49 +25,54 @@ const addImagestoGallery = (res) => {
       <span class="photo__likes">${element.likes}likes</span>
       <div class="photo__comments">
         <div class="photo__comment">
-          <span class="photo__comment-author">${element.user.username}</span>${element.alt_description}
+          <span class="photo__comment-author"><a href="profile.html?username=${element.user.username}">${element.user.username}</span></a>${element.alt_description}
         </div>
       </div>
     </div> 
     </section>   
+              
     `;
+  });
+  gallery.innerHTML = html;
+};
+
+const callAPI = async (keyword) => {
+  try {
+    console.log("keyword --> ", keyword);
+    const response = await fetch("/api/searchPhotos", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ keyword }),
     });
-    gallery.innerHTML = html;
+    const res = await response.json();
+    //check response return from our API
+    console.log("response ----> ", res);
+    addImagestoGallery(res);
+  } catch (error) {
+    console.log("message error --->", error);
+    //Do Something
+  }
 };
-const callAPI = async(keyword) => {
-    try {
-        console.log("keyword --> ", keyword);
-        const response = await fetch("/api/searchPhotos", {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ keyword }),
-        });
-        const res = await response.json();
-        //check response return from our API
-        console.log("response ----> ", res);
-        addImagestoGallery(res);
-    } catch (error) {
-        console.log("message error --->", error);
-        //Do Something
-    }
-};
+
 const removeAllPhoto = () => {
-    const galleryElement = document.querySelector(".gallery");
-    galleryElement.innerHTML = "";
+  const galleryElement = document.querySelector(".gallery");
+  galleryElement.innerHTML = "";
 };
+
 const searchPhoto = (event) => {
-    const keyword = event.target.value;
-    if (event.key === "Enter" && keyword) {
-        removeAllPhoto();
-        //5. Call API
-        callAPI(keyword);
-    }
+  const keyword = event.target.value;
+  if (event.key === "Enter" && keyword) {
+    removeAllPhoto();
+    //5. Call API
+    callAPI(keyword);
+  }
 };
 const main = () => {
-    const inputElement = document.querySelector(".search");
-    inputElement.addEventListener("keydown", searchPhoto);
+  const inputElement = document.querySelector(".search");
+  inputElement.addEventListener("keydown", searchPhoto);
 };
+
 main();
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,43 @@ const router = express.Router();
 //3. Configuration here
 const dotenv = require('dotenv').config();
 const Unsplash = require('unsplash-js').default;
-const { toJson } = require('unsplash-js');
-const unsplash = new Unsplash({ accessKey: process.env.myAccessKey });
+const {toJson} = require('unsplash-js');
+const unsplash = new
+Unsplash({accessKey:process.env.myAccessKey});
+
+
 
 //4. add API here
+router.post('/api/searchPhotos', (req,res)=>{
+    //PUT YOUR CODE HERE
+    let {keyword} = req.body;
+    unsplash.search.photos(keyword,1,21)
+    .then(toJson)
+    .then(json =>{
+       res.status(200).send(json)
+    })
+    .catch((err)=>{
+        console.log("Error Message -->", err);
+        res.status(200).send({"messageError":err})
+    });
+
+});
+
+router.post('/api/searchUser', (req,res)=>{
+    //PUT YOUR CODE HERE
+    let {username} = req.body;
+    unsplash.users.profile(username)
+    .then(toJson)
+    .then(json =>{
+       res.status(200).send(json)
+    })
+    .catch((err)=>{
+        console.log("Error Message -->", err);
+        res.status(200).send({"messageError":err})
+    });
+
+});
+
 router.post('/api/showImage', (req, res) => {
     let { username } = req.body;
     unsplash.users.photos(username, 1, 10, "latest")
@@ -22,32 +55,5 @@ router.post('/api/showImage', (req, res) => {
             })
         });
 });
-
-router.post('/api/searchUser', (req, res) => {
-    let { username } = req.body;
-    unsplash.users.profile(username)
-    .then(toJson)
-        .then(json => {
-            res.status(200).send(json)
-        })
-        .catch((err) => {
-            console.log("Error Message -->", err);
-            res.status(200).send({ "messageError": err })
-        });
-});
-
-router.post('/api/searchPhotos', (req, res) => {
-    let { keyword } = req.body;
-    unsplash.search.photos(keyword, 1, 21)
-        .then(toJson)
-        .then(json => {
-            res.status(200).send(json)
-        })
-        .catch((err) => {
-            console.log("Error Message -->", err);
-            res.status(200).send({ "messageError": err })
-        });
-});
-
 
 module.exports = router;
